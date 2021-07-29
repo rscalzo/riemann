ae2f8b430c732b72b8d7f145556f5f4682b84eee
Richard Scalzo <richard.a.scalzo@gmail.com>
2021-07-29 17:38:04 +1000

    Break out adaptation algorithms to adaptive.py, refactor randomwalk.py

--- riemann/proposals/test_randomwalk.py
+++ riemann/proposals/test_randomwalk.py
@@ -4,22 +4,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from riemann import Sampler
-from riemann.proposals.randomwalk import AdaptiveMetropolisRandomWalk
+from riemann.proposals.randomwalk import AdaptCovRandomWalk
+from riemann.proposals.randomwalk import AdaptScaleRandomWalk
+from riemann.proposals.randomwalk import AdaptScaleCovRandomWalk
+from riemann.proposals.randomwalk import pCN, AdaptScalepCN
 from riemann.models.benchmarks import benchmark_gauss2d_corr
 
 
-def testAdaptiveMetropolisRandomWalk():
+def testRandomWalk():
     """
     quick run of adaptive Metropolis through its paces
     :return: nothing
     """
-    proposal = AdaptiveMetropolisRandomWalk(0.1*np.eye(2), t_adapt=0,
-                                            smooth_adapt=False)
+    C0 = 0.0001*np.eye(2)
+    proposal = AdaptCovRandomWalk(C0, t_adapt=0, smooth_adapt=False)
+    """
+    proposal = AdaptScaleRandomWalk(C0)
+    proposal = AdaptScaleCovRandomWalk(C0, t_adapt=0, smooth_adapt=False)
+    C0 = np.eye(2)
+    proposal = pCN(C0, 0.5)
+    proposal = AdaptScalepCN(C0, 0.5)
+    """
     sampler = Sampler(benchmark_gauss2d_corr, proposal, np.ones(2))
     sampler.run(10000, 1000, 1)
     chain = np.array(sampler._chain_thetas)
     print("chain =", chain)
+    print("proposal.scale =", proposal.scale)
+    print("chain.acceptrate =", np.mean(chain[:-1] != chain[1:]))
     # let's see how we did
+    plt.figure(figsize=(6,8))
+    plt.subplot(2,1,1)
     plt.plot(chain[:,0], chain[:,1], marker='o', ls='None')
+    plt.subplot(2,1,2)
+    plt.plot(chain)
     plt.show()
     return chain
